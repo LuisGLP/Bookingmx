1ebe91f3cc70cdbf781d8fb7e93aae66f398a2f0
Alberto García <galoop8@gmail.com>
2025-11-15 23:36:50 -0600

    Add Global Exception Handler

---
+++ backend/src/main/java/com/bookingmx/reservations/controller/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.bookingmx.reservations.controller;
+
+import com.bookingmx.reservations.exception.BadRequestException;
+import com.bookingmx.reservations.exception.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleNotFound(NotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler({ BadRequestException.class, MethodArgumentNotValidException.class, HttpMessageNotReadableException.class })
+    public ResponseEntity<String> handleBadRequest(Exception ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleOther(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal error");
+    }
+}
+
