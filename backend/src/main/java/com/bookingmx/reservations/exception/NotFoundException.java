0594e277ad582ffd3b8d4059172666004253abd7
Alberto García <galoop8@gmail.com>
2025-11-18 12:57:04 -0600

    Update NotFoundException.java

--- backend/src/main/java/com/bookingmx/reservations/exception/NotFoundException.java
+++ backend/src/main/java/com/bookingmx/reservations/exception/NotFoundException.java
@@ -5,20 +5,19 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * NotFoundException
- *
  * This custom exception is thrown when a requested resource cannot be found.
  * The @ResponseStatus annotation ensures that Spring returns an HTTP 404 NOT FOUND
  * response whenever this exception is triggered.
  */
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
-
     /**
      * Constructor for NotFoundException.
      *
      * @param m A descriptive error message indicating which resource was not found.
-     *
      * This message is included in the HTTP response body so the client
      * can understand the cause of the error.
      */
-    public NotF
+    public NotFoundException(String m) { super(m); }
+}
+
