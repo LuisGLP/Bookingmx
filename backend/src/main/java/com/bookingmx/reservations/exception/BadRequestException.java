3ec30317205197554820c70567166b95a449c70a
SeleneGuerrero <147877261+SeleneGuerrero@users.noreply.github.com>
2025-11-22 16:03:39 -0600

    Update BadRequestException.java

--- backend/src/main/java/com/bookingmx/reservations/exception/BadRequestException.java
+++ backend/src/main/java/com/bookingmx/reservations/exception/BadRequestException.java
@@ -3,7 +3,26 @@ package com.bookingmx.reservations.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * BadRequestException
+ *
+ * This custom exception is used to indicate that the client has sent
+ * an invalid or malformed request. When this exception is thrown,
+ * Spring automatically returns an HTTP 400 BAD REQUEST response because
+ * of the @ResponseStatus annotation.
+ */
+
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BadRequestException extends RuntimeException {
+
+    /**
+     * Constructor for BadRequestException.
+     *
+     * @param m The error message describing why the request is invalid.
+     *
+     * This message is included in the HTTP response body and helps
+     * the client understand what went wrong.
+     */
+
     public BadRequestException(String m) { super(m); }
 }
